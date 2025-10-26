0627d0cfca0b400e0638f1a2f8ec9f0644b0bdc1
lsk9504 <wavecanon3@gmail.com>
2025-10-26 10:38:30 +0900

    requierments upload

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)cdp!y$j+&^*o-hktmm(+$%^%9_8e-rn6$6$fw2&_q!f+9n)bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leecase.pythonanywhere.com']
 
 
 # Application definition
